d848e50c0587785a177c96588921551b4c3b9547
er4830 <er4830@gmail.com>
2023-03-02 03:07:11 -0800

    fixed Task104

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,50 +2,51 @@
 #include <stdio.h>
 
 /**
- * print_buffer - Prints a buffer
- * @b: char
- * @size: int
- * Return:void
+ * print_buffer - prints buffer
+ * @b: buffer
+ * @size: size
+ * Return: void
  */
+
 void print_buffer(char *b, int size)
 {
-	int x, i;
+	int o, j, i;
 
-	for (x = 0; x < size; x += 10)
-	{
-		printf("%08x: ", x);
-
-		for (i = 0; i < 10; i++)
-		{
-			if ((i + x) >= size)
-				printf("  ");
-
-			else
-				printf("%02x", *(b + i + x));
-
-			if ((i % 2) != 0 && i != 0)
-				printf(" ");
-		}
-
-		for (i = 0; i < 10; i++)
-		{
-			if ((i + x) >= size)
-				break;
-
-			else if (*(b + i + x) >= 31 &&
-				 *(b + i + x) <= 126)
-				printf("%c", *(b + i + x));
-
-			else
-				printf(".");
-		}
-
-		if (x >= size)
-			continue;
-
-		printf("\n");
-	}
+	o = 0;
 
 	if (size <= 0)
+	{
 		printf("\n");
-}	
+		return;
+	}
+	while (o < size)
+	{
+		j = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
+		for (i = 0; i < 10; i++)
+		{
+			if (i < j)
+				printf("%02x", *(b + o + i));
+			else
+				printf("  ");
+			if (i % 2)
+			{
+				printf(" ");
+			}
+		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
+	}
+}
+
+
